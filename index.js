7778eaaf12732f6e38749ee864de4433e95ee1b7
binsu77 <binsuhaneef@gmail.com>
2025-03-04 02:46:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './server.js';
+import server from './server.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <server/>
   </React.StrictMode>
 );
 
